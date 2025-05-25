7b82de1812b8bfb8d8abc3296fe5f89875567c62
lngd <lngdeer@gmail.com>
2025-05-25 17:31:12 +0300

    single ups concept

--- README.md
+++ README.md
@@ -2,7 +2,11 @@ flask-overseer
 ========
 ``flask`` based app for monitoring and controlling
 
+requirements
+------------
+* ``flask[async]``
+* ``pysnmp`` (with compiled MIB in ~/pysnmp_mibs or site-packages/pysnmp/smi/mibs)
 
-Installation
+installation
 ------------
 ``https://github.com/longdeer/flask-overseer.git``
--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,5 @@
 import	os
 from	typing						import List
-from	time						import sleep
 from	flask						import Flask
 from	flask						import render_template
 from	pysnmp.hlapi.v3arch.asyncio	import SnmpEngine
@@ -10,7 +9,6 @@ from	pysnmp.hlapi.v3arch.asyncio	import UdpTransportTarget
 from	pysnmp.hlapi.v3arch.asyncio	import ContextData
 from	pysnmp.hlapi.v3arch.asyncio	import ObjectType
 from	pysnmp.hlapi.v3arch.asyncio	import ObjectIdentity
-from	pysnmp.smi					import builder
 
 
 
@@ -19,29 +17,16 @@ from	pysnmp.smi					import builder
 
 
 
-# mib_builder = builder.MibBuilder()
-# mib_sources = mib_builder.get_mib_sources() + ( builder.DirMibSource(os.path.join(os.getcwd(),"snmp")), )
-# mib_builder.set_mib_sources(*mib_sources)
-# mib_builder.load_modules("XPPC-MIB")
+app = Flask(__name__)
 
 
 
 
-
-
-
-
-async def eltena3kcc() -> List[str] :
+@app.route("/get_snmp_eltena")
+async def get_snmp_eltena():
 
 	engine = SnmpEngine()
-	# mib_builder = engine.get_mib_builder()
-	# mib_builder.add_mib_sources(builder.DirMibSource())
-	# mib_builder.set_mib_sources(mib_builder.get_mib_sources() + ( builder.DirMibSource(os.path.join(os.getcwd(),"snmp")), ))
-	# engine.get_mib_builder().set_mib_sources(*mib_sources)
-	# engine.get_mib_builder().load_modules("XPPC-MIB")
-	# mib_builder = builder.MibBuilder()
-	# mib_builder.load_modules("XPPC-MIB")
-	result = [ "None" ] *6
+	data = [ "None" ] *6
 
 	errorIndication, errorStatus, errorIndex, varBinds = await get_cmd(
 
@@ -49,12 +34,8 @@ async def eltena3kcc() -> List[str] :
 		CommunityData("public", mpModel=0),
 		await UdpTransportTarget.create(("192.168.160.253", 161)),
 		ContextData(),
-		# ObjectType(ObjectIdentity("XPPC-MIB", "upsSmartInputLineVoltage", 0)).loadMibs("XPPC-MIB"),
-		ObjectType(ObjectIdentity(
-
-			"XPPC-MIB", "upsSmartInputLineVoltage", 0)
-
-		).addMibSource(os.path.join(os.getcwd(),"snmp")),
+		ObjectType(ObjectIdentity("XPPC-MIB", "upsSmartInputLineVoltage", 0)),
+		ObjectType(ObjectIdentity("XPPC-MIB", "upsSmartInputLineVoltage", 0)),
 		ObjectType(ObjectIdentity("XPPC-MIB", "upsSmartBatteryVoltage", 0)),
 		ObjectType(ObjectIdentity("XPPC-MIB", "upsSmartOutputLoad", 0)),
 		ObjectType(ObjectIdentity("XPPC-MIB", "upsSmartBatteryCapacity", 0)),
@@ -72,40 +53,13 @@ async def eltena3kcc() -> List[str] :
 		)
 	)
 	else:
-		result = [ value.prettyPrint() for _,value in varBinds ]
+		data = [ value.prettyPrint() for _,value in varBinds ]
 
 
 	engine.close_dispatcher()
-	return result
+	return render_template("viewer.html", data=data)
 
 
-
-
-
-
-
-
-app = Flask(__name__)
-
-
-@app.route("/eltena_fetcher")
-async def fetcher():
-
-	data = await eltena3kcc()
-	return render_template(
-
-		"viewer.html",
-		# headers=[
-
-		# 	"Input Voltage",
-		# 	"Battery Voltage",
-		# 	"Output load",
-		# 	"Capacity",
-		# 	"Temperature",
-		# 	"Input frequency",
-		# ],
-		data=data
-	)
 
 
 @app.route("/eltena<upd>")
