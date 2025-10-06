230c9e118781e220b3c50a08bc49a87c9f225a44
lngd <lngdeer@gmail.com>
2025-10-06 16:11:04 +0300

    Reader front bad concept

--- client/reader.js
+++ client/reader.js
@@ -9,30 +9,129 @@
 function initReader() {
 
 	const reader = document.getElementsByClassName("reader-view")[0];
+	const treeMap = new Map();
+	const expanded = new Map();
 	// let   messageBlock;
 
 
-	fetch("/reader-setup")
-	.then(response => {
+	// fetch("/reader-setup")
+	// .then(response => {
 
-		if(response.status !== 200) console.error(`setup fetch status: ${response.status}`);
-		else response.json().then(data => {
+	// 	if(response.status !== 200) console.error(`setup fetch status: ${response.status}`);
+	// 	else response.json().then(data => {
 
-			// data.forEach(message => {
+	// 		// data.forEach(message => {
 
-			// 	messageBlock = document.createElement("pre");
-			// 	messageBlock.className = "announcer-message";
-			// 	messageBlock.innerText = message;
-			// 	announcer.appendChild(messageBlock)
-			// });	announcer.scrollIntoView(false);//window.scrollTo(0, document.body.scrollHeight)
+	// 		// 	messageBlock = document.createElement("pre");
+	// 		// 	messageBlock.className = "announcer-message";
+	// 		// 	messageBlock.innerText = message;
+	// 		// 	announcer.appendChild(messageBlock)
+	// 		// });	announcer.scrollIntoView(false);//window.scrollTo(0, document.body.scrollHeight)
 
-			console.log(data)
-		})
-	})
-	.catch(E => console.error(E));
+	// 		console.log(data)
+	// 	})
+	// })
+	// .catch(E => console.error(E));
 
 
-	// new WebSocket(`ws://${location.host}/announcer-wscast`).onmessage = event => {
+	const ws = new WebSocket(`ws://${location.host}/reader-wscast`);
+	// ws.addEventListener("open",event => console.log(event));
+	ws.addEventListener("message",event => {
+
+		const data = JSON.parse(event.data);
+		if(data.roots) {
+
+			console.log(data.roots);
+			const rootList = document.createElement("ul");
+			data.roots.forEach(path => {
+
+				const root = document.createElement("li");
+				root.innerText = path;
+				root.className = "reader-folder-item";
+				root.addEventListener("click",event => {
+					console.log(event.target)
+
+					event.preventDefault();
+					if(expanded.has(event.target)) {
+
+						// expanded.set(event.target, !expanded.get(event.target));
+						// event.target.style.display = expanded.get(event.target) ? "grid" : "none"
+						console.log(`in roots ${event.target.children}`)
+
+					}	else {
+
+						expanded.set(event.target, true);
+						ws.send(event.target.innerText)
+					}
+				});
+
+				rootList.appendChild(root);
+				treeMap.set(path, root)
+			});
+
+			reader.appendChild(rootList)
+
+		}	else if(data.parent && data.children) {
+
+			const list = document.createElement("ul");
+			const parent = data.parent;
+			console.log(`parent = ${parent}`);
+			console.log(`children = ${data.children}`);
+
+			data.children.folders.forEach(path => {
+
+				const child = document.createElement("li");
+				child.innerText = path;
+				child.className = "reader-folder-item";
+				child.addEventListener("click",event => {
+
+					event.preventDefault();
+					if(expanded.has(event.target)) {
+
+						// expanded.set(event.target, !expanded.get(event.target));
+						// event.target.style.display = expanded.get(event.target) ? "grid" : "none"
+						console.log(`if folders ${event.target.children}`)
+
+					}	else {
+
+						expanded.set(event.target, true);
+						ws.send(event.target.innerText)
+					}
+				});
+
+				list.appendChild(child);
+				treeMap.set(path, child)
+			});
+			data.children.files.forEach(path => {
+
+				const child = document.createElement("li");
+				child.innerText = path;
+				child.className = "reader-file-item";
+				child.addEventListener("click",event => {
+
+					event.preventDefault();
+					if(expanded.has(event.target)) {
+
+						// expanded.set(event.target, !expanded.get(event.target));
+						// event.target.style.display = expanded.get(event.target) ? "grid" : "none"
+						console.log(`in files ${event.target.children}`)
+
+					}	else {
+
+						expanded.set(event.target, true);
+						ws.send(event.target.innerText)
+					}
+				});
+
+				list.appendChild(child);
+				treeMap.set(path, child)
+			});
+
+			treeMap.get(parent).appendChild(list)
+
+		}	else console.error("Improper data")
+	});
+	// ws.onmessage = event => {
 
 	// 	messageBlock = document.createElement("pre");
 	// 	messageBlock.className = "announcer-message";
@@ -50,6 +149,10 @@ function initReader() {
 	// 	announcer.scrollIntoView(false)
 	// }
 }
+// function toggle() {
+
+// 	return
+// }
 
 
 
